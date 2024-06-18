ceb5e3176328c91c4f838fda8669794ac79b3613
Top-CrazyDev <kevinjohn00318@gmail.com>
2024-06-19 04:56:55 +0900

    initial commit

---
+++ src/data/places.ts
@@ -0,0 +1,293 @@
+import { IPin, EPinType } from 'src/lib/types'
+
+export const PLACES: IPin[] = [
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Coffee,
+    title : 'La Central',
+    city: 'Barcelona',
+    country: 'Spain',
+    coordinates: [41.38331985473633, 2.168530225753784],
+    date: '2024-02-08',
+    photo: '/photos/JuditKaramazov/barcelona-la-central.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Picture,
+    title : 'Parque de la Ciutadella',
+    city: 'Barcelona',
+    country: 'Spain',
+    coordinates: [41.383941650390625, 2.1787397861480713],
+    date: '2024-06-04',
+    photo: '/photos/AuNedelec/la_ciutadella.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title : 'Kahiki',
+    city: 'Barcelona',
+    country: 'Spain',
+    coordinates: [41.3888, 2.159],
+    date: '2024-05-18',
+    photo: '/photos/AuNedelec/kahiki.jpg'
+  },
+{
+  author: 'Auregan Nedelec',
+  username: 'AuNedelec',
+  type: EPinType.Coffee,
+  title : 'Mescladís del Pou',
+  city: 'Barcelona',
+  country: 'Spain',
+  coordinates: [41.3870865, 2.1803497],
+  date: '2024-06-04',
+  photo: '/photos/AuNedelec/mescladis_del_pou.jpg'
+},
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Event,
+    title : 'Museu Nacional d\'Art de Catalunya',
+    city: 'Montjuïc, Barcelona',
+    country: 'Spain',
+    coordinates: [41.36857223510742, 2.1534512042999268],
+    date: '2024-05-25',
+    photo: '/photos/AuNedelec/museum.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title : 'Les gens que j\'aime',
+    city: 'Barcelona',
+    country: 'Spain',
+    coordinates: [41.3888, 2.159],
+    date: '2024-05-25',
+    photo: '/photos/AuNedelec/les_gens_que_jaime.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title : 'Hidden Coffee Roasters',
+    city: 'Barcelona',
+    country: 'Spain',
+    coordinates: [41.38291931152344, 2.1822259426116943],
+    date: '2024-06-10',
+    photo: '/photos/AuNedelec/hidden_coffee_roasters.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Picture,
+    title : 'Castell de Montjuïc',
+    city: 'Montjuïc, Barcelona',
+    country: 'Spain',
+    coordinates: [41.3687339, 2.1690936],
+    date: '2024-05-18',
+    photo: '/photos/AuNedelec/montjuic.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Home,
+    city: 'Engelhartszell an der Donau',
+    country: 'Austria',
+    coordinates: [48.5070, 13.7321],
+    date: '2021-12-03',
+    photo: '/photos/JuditKaramazov/engel.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Event,
+    title: 'Schloss Ort',
+    city: 'Gmunden',
+    country: 'Austria',
+    coordinates: [47.911659240722656, 13.7923583984375],
+    date: '2022-01-16',
+    photo: '/photos/JuditKaramazov/gmunden.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Home,
+    city: 'Granollers',
+    country: 'Spain',
+    coordinates: [41.598167419433594, 2.283038854598999],
+    date: '2024-04-07',
+    photo: '/photos/JuditKaramazov/granollers.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title : "Palo Santo",
+    city: 'Granollers',
+    country: 'Spain',
+    coordinates: [41.6047293, 2.2863076],
+    date: '2024-06-01',
+    photo: '/photos/AuNedelec/palo_santo.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Picture,
+    city: 'Krakóv',
+    country: 'Poland',
+    coordinates: [50.049683, 19.944544],
+    date: '2022-06-04',
+    photo: '/photos/AuNedelec/krakow.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title : 'Europeska',
+    city: 'Krakóv',
+    country: 'Poland',
+    coordinates: [50.062374114990234, 19.9366512298584],
+    date: '2022-06-04',
+    photo: '/photos/AuNedelec/europeska_krakow.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Event,
+    title: 'Fête des Lumières',
+    city: 'Lyon',
+    country: 'France',
+    coordinates: [45.7640, 4.8357],
+    date: '2022-12-11',
+    photo: '/photos/JuditKaramazov/lyon-fete-lumieres.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title : 'Lyon Particule',
+    city: 'Lyon',
+    country: 'France',
+    coordinates: [45.761122, 4.8280905],
+    date: '2022-09-18',
+    photo: '/photos/AuNedelec/lyon_particule.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Coffee,
+    title: 'Caffè il Forno',
+    city: 'Mataró',
+    country: 'Spain',
+    coordinates: [41.5381, 2.4447],
+    date: '2024-04-15',
+    photo: '/photos/JuditKaramazov/mataro.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title: 'Can Margarit',
+    city: 'Mataró',
+    country: 'Spain',
+    coordinates: [41.53901672363281, 2.453969717025757],
+    date: '2024-05-06',
+    photo: '/photos/AuNedelec/can_margarit_mataro.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Picture,
+    city: 'Paris',
+    country: 'France',
+    coordinates: [48.85785675048828, 2.3009138107299805],
+    date: '2024-05-03',
+    photo: '/photos/AuNedelec/paris-eiffel.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title: 'Le Triomphe',
+    city: 'Paris',
+    country: 'France',
+    coordinates: [48.84734344482422, 2.395885944366455],
+    date: '2024-05-03',
+    photo: '/photos/AuNedelec/paris.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Picture,
+    city: 'Passau',
+    country: 'Germany',
+    coordinates: [48.5748229, 13.4609744],
+    date: '2021-12-04',
+    photo: '/photos/JuditKaramazov/passau.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Picture,
+    city: 'Roma',
+    country: 'Italy',
+    coordinates: [41.889048, 12.492502],
+    date: '2022-03-29',
+    photo: '/photos/JuditKaramazov/roma-colosseo.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Coffee,
+    title: 'Augustiner Bräu Mülln',
+    city: 'Salzburg',
+    country: 'Austria',
+    coordinates: [47.80522537231445, 13.032347679138184],
+    date: '2022-01-14',
+    photo: '/photos/JuditKaramazov/salzburg-augustiner.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Picture,
+    city: 'Schärding',
+    country: 'Austria',
+    coordinates: [48.45577621459961, 13.431777954101562],
+    date: '2022-01-14',
+    photo: '/photos/JuditKaramazov/scharding.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title : 'Mont Roig Café',
+    city: 'Sitges',
+    country: 'Spain',
+    coordinates: [41.23596954345703, 1.806681752204895],
+    date: '2024-06-05',
+    photo: '/photos/AuNedelec/mont_roig_cafe.jpg'
+  },
+  {
+    author: 'Auregan Nedelec',
+    username: 'AuNedelec',
+    type: EPinType.Coffee,
+    title : 'La formatgeria de Sitges',
+    city: 'Sitges',
+    country: 'Spain',
+    coordinates: [41.2375727,1.8113308],
+    date: '2024-06-05',
+    photo: '/photos/AuNedelec/la_formatgeria.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Picture,
+    city: 'Vienne',
+    country: 'France',
+    coordinates: [45.5256, 4.8743],
+    date: '2022-12-08',
+    photo: '/photos/JuditKaramazov/vienne-saint-maurice.jpg'
+  }
+]
